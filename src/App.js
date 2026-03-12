46f1565912891186bd6b0e5aa91cb0e99ea575e1
PreshaShrestha4 <preshashrestha041@gmail.com>
2026-03-12 14:43:21 +0545

    updated all

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
@@ -6,20 +6,26 @@ import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Courses from "./pages/Courses";
+import CoursePage from "./pages/CoursePage"; // dynamic course page
 import Contact from "./pages/Contact";
+
+import "./index.css";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Navbar />
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/courses" element={<Courses />} />
+        <Route path="/courses/:courseId" element={<CoursePage />} />{" "}
         <Route path="/contact" element={<Contact />} />
       </Routes>
+
       <Footer />
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
