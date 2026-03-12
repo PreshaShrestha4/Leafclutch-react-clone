745be7ddfb2848167d48fb38fb14df6e345d2b33
PreshaShrestha4 <preshashrestha041@gmail.com>
2026-03-12 14:41:04 +0545

    Updated contact pages

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,20 +1,12 @@
+import ContactHero from "../components/ContactHero";
+import ContactSection from "../components/ContactSection";
+
 function Contact() {
   return (
-    <section className="contact container">
-      <h2>Contact Us</h2>
-      <form>
-        <label>Name</label>
-        <input type="text" placeholder="Your Name" />
-
-        <label>Email</label>
-        <input type="email" placeholder="Your Email" />
-
-        <label>Message</label>
-        <textarea placeholder="Your Message"></textarea>
-
-        <button type="submit">Send</button>
-      </form>
-    </section>
+    <>
+      <ContactHero />
+      <ContactSection />
+    </>
   );
 }
 
